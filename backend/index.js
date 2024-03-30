109194456ec51174c53d8e2ed4db3975f01f3de2
Jigyashu <jigyashusingh01@gmail.com>
2024-03-30 21:42:37 +0530

    update

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 app.use(cors(
     {
-        origin: '*',
+        origin: 'https://portfolio-wyng.onrender.com',
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     }
 ));
